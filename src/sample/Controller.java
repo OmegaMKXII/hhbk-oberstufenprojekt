39b507415d6c8d1353a9a5903310a8dfe9dc9c81
Monika <monika_pols@hotmail.de>
2017-05-02 09:14:22 +0200

    login-mask

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    Label loginLabel;
+
+    @FXML
+    Label benutzerLabel;
+
+    @FXML
+    Label passwortLabel;
+
+    @FXML
+    TextField benutzerTextfield;
+
+    @FXML
+    TextField passwortTextfield;
+
+    @FXML
+    Button weiterButton;
+
+
+    public void weiterButtonEvent(MouseEvent mouseEvent) {
+    }
 }
