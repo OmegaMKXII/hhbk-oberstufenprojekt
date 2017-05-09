f9a0b1c94f9bcdd07399c0306de972f88db46c4c
x1n4u <x1n4u@protonmail.com>
2017-05-09 13:05:09 +0200

    Added SQL QUERY for years by skill

--- src/main/java/de/hhbk/wizardpdfgen/model/persistence/sql/DidacticWizardDAO.java
+++ src/main/java/de/hhbk/wizardpdfgen/model/persistence/sql/DidacticWizardDAO.java
@@ -17,7 +17,11 @@ public class DidacticWizardDAO {
     private static Logger logger = LogManager.getLogger(DidacticWizardDAO.class);
 
     // MySQL Statement
-    private static final String GET_ALL_SKILLED_OCCUPATION= "SELECT tbl_beruf.Berufname FROM `tbl_beruf`";
+    private static final String GET_ALL_SKILLED_OCCUPATION= "SELECT tbl_beruf.berufname FROM tbl_beruf";
+    private static final String GET_ALL_YEARS_FOR_SKILLED_OCCUPATION= "SELECT bf.jahr FROM tbl_beruffach as bf " +
+            "join tbl_uformberuf as ub on ub.ubid = bf.id_uformberuf " +
+            "join tbl_beruf as b on b.bid = ub.id_beruf " +
+            "where b.berufname = ?;";
 
     /**
      * Retrieves a list of all skilled occupation in database of didactic wizard application
@@ -59,11 +63,33 @@ public class DidacticWizardDAO {
      * Retrieves all training years of given skilled occupation
      * @param skilledOccupation
      */
-    public List<Integer> getTrainingYearsBySkilledOccupation(String skilledOccupation){
+    public List<Integer> getTrainingYearsBySkilledOccupation(String skilledOccupation) throws SQLException {
         List<Integer> trainingYearList = new ArrayList<Integer>();
-        //TODO
 
-
+        Connection conn = null;
+        try {
+            conn = DBUtil.getConnection(DBType.DIDACTSQL_DB);
+            PreparedStatement statement = conn.prepareStatement(GET_ALL_YEARS_FOR_SKILLED_OCCUPATION);
+            statement.setString(1,skilledOccupation);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                trainingYearList.add(rs.getInt(1));
+            }
+        } catch (SQLException e) {
+            String errMsg = "Error while connecting to database";
+            logger.error(errMsg);
+            throw new SQLException(errMsg, e);
+        } finally {
+            try {
+                if(conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                String errMsg = "Error while closing the connection to database";
+                logger.warn(errMsg);
+                throw new SQLException(errMsg, e);
+            }
+        }
         return trainingYearList;
     }
 
